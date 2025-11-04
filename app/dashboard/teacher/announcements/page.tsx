bd74e2450652a5edcb4aaa74fe96263074e3092f
Rishabhpal07 <rishabhpal21390@gmail.com>
2025-11-04 10:20:56 +0530

    final done

---
+++ app/dashboard/teacher/announcements/page.tsx
@@ -0,0 +1,12 @@
+import AnnouncementList from '@/components/announcement/AnnouncementList'
+import React from 'react'
+
+function page() {
+  return (
+    <div className='p-10'>
+     <AnnouncementList/>
+    </div>
+  )
+}
+
+export default page
--- app/dashboard/teacher/page.tsx
+++ app/dashboard/teacher/page.tsx
@@ -1,9 +1,13 @@
+import { UpcomingClasses } from '@/components/UpcomingClasses'
+import { WeeklyTimetable } from '@/components/WeeklyTimetable'
 import React from 'react'
 
 function page() {
   return (
-    <div>
-      teacher
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Your Class Timetable</h1>
+      <UpcomingClasses/>
+      <WeeklyTimetable/>
     </div>
   )
 }
